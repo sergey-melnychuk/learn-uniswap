c133e3d73d0ed936bfc073d7b7b41692a6551a6d
Sergey Melnychuk <sergey-melnychuk@users.noreply.github.com>
2024-12-18 18:47:14 +0100

    .

--- arbitrage/src/Worker.sol
+++ arbitrage/src/Worker.sol
@@ -5,4 +5,5 @@ import { UniswapV2Pair } from "@uniswap-v2/contracts/UniswapV2Pair.sol";
 
 contract Worker {
     // TODO
+    // https://docs.uniswap.org/contracts/v2/guides/smart-contract-integration/using-flash-swaps
 }
